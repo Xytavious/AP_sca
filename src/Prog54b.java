105e76dca4af57f37f674e441f8beb3f1445bf21
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-09-04 11:30:17 -0500

    hi2

--- src/Prog54b.java
+++ src/Prog54b.java
@@ -1,6 +1,30 @@
 import java.util.*;
 public class Prog54b {
     public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Enter First Number: ");
+        int num1 = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter Second Number: ");
+        int num2 = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter Number: ");
+        int num3 = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter Number: ");
+        int num4 = input.nextInt();
+        System.out.println();
+
+        int tot = num1 + num2 + num3 + num4;
+        double avg = tot / 4;
+
+
+        System.out.println("sum = " + tot);
+        System.out.println("average = " + avg);
 
     }
 }
