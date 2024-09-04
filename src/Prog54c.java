284a63a1260af560dd1d49d0a15996d6040e599a
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-09-04 11:38:10 -0500

    day 2

--- src/Prog54c.java
+++ src/Prog54c.java
@@ -1,2 +1,8 @@
+import java.util.*;
 public class Prog54c {
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Enter the Radius of the Circle");
+    }
 }
