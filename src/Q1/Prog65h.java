979e7910450794d9ae19f45ea8f2dfb97e02d6e5
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-09-10 11:25:57 -0500

    day2 prog54s done

---
+++ src/Q1/Prog65h.java
@@ -0,0 +1,29 @@
+package Q1;
+import java.util.*;
+public class Prog65h {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Pounds ");
+        int pound = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter Shillings ");
+        int shilly = input.nextInt();
+        System.out.println();
+
+        System.out.print("Enter pence ");
+        int pence = input.nextInt();
+        System.out.println();
+
+        /*
+        1 shilling = 12 pence
+        20 shillings = 1 pound
+        convert to only
+        pounds and pence
+        */
+        if (shilly == 12 )
+            else;
+
+    }
+}
