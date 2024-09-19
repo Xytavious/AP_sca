bea44d3c68ada58add6ccb6ef3835a86e522a060
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-09-19 11:26:55 -0500

    9/19/2024

--- src/Q1/Lang93a.java
+++ src/Q1/Lang93a.java
@@ -5,6 +5,20 @@ public class Lang93a {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter Kilowatts Used ");
+        double kw = input.nextDouble();
+        System.out.println();
+
+        double bill = kw * 0.0475;
+        double sur = (bill/10);
+        double CyTax = (bill* 0.03);
+        double tot = bill + sur + CyTax;
+        double Late = tot * 0.04;
+        double lateTOT = tot + Late;
+
+        System.out.println("Base Rate " + kw + "@  $0.0475" );
+
+
+
 
     }
 }
