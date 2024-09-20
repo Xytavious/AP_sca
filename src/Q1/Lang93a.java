e03d24abec5307939d9d91834f0ae220420348e2
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-09-20 11:26:18 -0500

    9/20/2024

--- src/Q1/Lang93a.java
+++ src/Q1/Lang93a.java
@@ -15,10 +15,26 @@ public class Lang93a {
         double Late = tot * 0.04;
         double lateTOT = tot + Late;
 
-        System.out.println("Base Rate " + kw + "@  $0.0475" );
+        System.out.println("Base Rate " + kw + " @  $0.0475 " + bill);
+        System.out.println("Surcharge " + sur);
+        System.out.print( "City Tax " + CyTax + "\n");
+        System.out.println("\n Pay this amount " + tot);
+        System.out.println("\n After May 20th Pay " + lateTOT);
 
 
 
 
     }
 }
+/*
+Enter Kilowatts Used 993
+
+Base Rate 993.0 @  $0.0475 47.167500000000004
+Surcharge 4.71675
+City Tax 1.415025
+
+ Pay this amount 53.299275
+
+ After May 20th Pay 55.431246
+
+ */
