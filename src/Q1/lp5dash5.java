b0e6992075aa649b39f891deae19792d60d646da
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-10-14 12:00:35 -0500

    10/14/2024

---
+++ src/Q1/lp5dash5.java
@@ -0,0 +1,11 @@
+package Q1;
+import java.util.Scanner;
+public class lp5dash5 {
+    public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
+        System.out.println("");
+        int sc = wow.nextInt();
+        System.out.println(sc.indexOf(0,));
+
+    }
+}
---
+++ src/Q1/prog93cl/prog93cl.java
@@ -0,0 +1,7 @@
+package Q1.prog93cl;
+
+public class prog93cl {
+    public static void main(String[] args) {
+        // Todo use scanner for input; remove code below
+    }
+}
