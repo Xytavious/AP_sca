f852bdc887e4b3b52ddd791d75e8d29943bb9573
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-11-05 12:00:57 -0600

    11/5/2024

---
+++ src/Q2/ProgArray/Cl213f.java
@@ -0,0 +1,19 @@
+package Q2.ProgArray;
+
+public class  Cl213f {
+    private double myKwh;
+    private double myCost;
+
+    public Cl213f(double Kwh){
+        myKwh = Kwh;
+        myCost=0;
+
+    }
+    //TODO:
+    public void calc(){}
+
+    public String toString(){
+        return "The cost of "; //TODO: complete
+
+    }
+}
