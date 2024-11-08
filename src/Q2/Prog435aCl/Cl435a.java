da42a9fa854db3b9878a756f0139d034d787aa00
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-11-08 12:01:21 -0600

    11/8/2024

--- src/Q2/Prog435aCl/Cl435a.java
+++ src/Q2/Prog435aCl/Cl435a.java
@@ -1,7 +1,20 @@
 package Q2.Prog435aCl;
 
 public class Cl435a {
-    private int gate;
+    private int gate1;
+    private int gate2;
+    private int gate3;
+    private int gate4;
+    private int gate5;
+    private int gate6;
+    private int gate7;
+    private int gate8;
     private int toll;
+    private int v;
+    private int tpye;
+
+    public void calc(int t,int ){
+
+    }
 
 }
