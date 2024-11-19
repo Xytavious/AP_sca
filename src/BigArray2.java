f5165fce36e84104e017e194ad0e57aa7b2b24e3
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-11-19 12:01:04 -0600

    11/19/2024

--- src/BigArray2.java
+++ src/BigArray2.java
@@ -2,6 +2,7 @@ import Q2.Cat;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class BigArray2 {
@@ -15,23 +16,24 @@ public class BigArray2 {
             Scanner file = new Scanner(new File("Langdat/bigarraylsit.dat"));
             int numCats = file.nextInt();
             file.nextLine();
-            Cat[] cats= new Cat[50];
-            for (int lcv =0; lcv < numCats; lcv++)
+            Cat[] cats = new Cat[50];
+
+            for (int lcv = 0; lcv < numCats; lcv++) {
                 String n = file.nextLine();
                 double w = file.nextDouble();
-                int a = file.nextInt();
+                int    a = file.nextInt();
                 double c = file.nextDouble();
                 file.nextLine();
-                cats[lcv] = new Cat(n,w,a,c);
-            System.out.println("");
-            System.out.println("");
-            for (int lcv = 0; lcv< numCats; lcv++)
-                printCat(cats[lcv]);
+                cats[lcv] = new Cat(n, w, a, c);
+                System.out.println("1. All the cats:");
+                System.out.println("Name\tWeight\tAge\tCost");
+                for (int lcv = 0; lcv < numCats; lcv++)
+                    printCat(cats[lcv]);
 
             System.out.println("\n2. The third cat is named: " + cats[2].getName());
 
             cats[numCats-1].setWeight(cats[numCats-1].getWeight()+10);
-            System.out.println("3. The updated weight is: " cats[numCats-1].getWeight());
+            System.out.println("3. The updated weight is: " + cats[numCats-1].getWeight());
 
             int rascalIndex = 0;
             for (int i =0; i< numCats; i++)
@@ -64,10 +66,22 @@ public class BigArray2 {
             cats[1] = cats[3];
             cats[3] = tempcat;
 
+            System.out.println("\n10. The current cat names are:");
+            for (Cat cat : cats)
+                if (cat != null) System.out.print(cat.getName() + "\t");
+            System.out.println();
+
             System.out.println("");
+            for (int i = 0; i < numCats; i ++)
+                Cat cat = cats[i];
+            if (cat.getWeight()>15)
+                System.out.println("");
 
         }catch (IOException e){
             System.out.println("Error: " + e.getStackTrace());
+
+            int k = 0;
+            while (k < numCats)
         }
     }
 }
