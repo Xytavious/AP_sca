dc0750195706d004b960f518144bec7677e2b70b
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-11-25 12:52:57 -0600

    11/25/2024

---
+++ src/Q2/Lp3dash9.java
@@ -0,0 +1,35 @@
+package Q2;
+import java.util.Scanner;
+public class Lp3dash9 {
+    public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
+        System.out.println("Enter Your Birthdate: ");
+        int Bday = wow.nextInt();
+
+        int years = 365 % 1;
+
+        System.out.println("Year:");
+        int year = 0;
+
+        System.out.println("Year:");
+        int month =0;
+
+        System.out.println("Year:");
+        int day =0;
+
+        System.out.println("Enter todays Date:");
+        int today = wow.nextInt();
+
+        System.out.println("Year:");
+        int Tyear = 0;
+
+        System.out.println("Year:");
+        int Tmonth =0;
+
+        System.out.println("Year:");
+        int Tday =0;
+
+         int ta =0;
+
+    }
+}
