4346399257c4c2e44e7e9e760effa53e5009aa25
williams.x <williams.x@stu.janesville.k12.wi.us>
2024-12-19 12:00:03 -0600

    12/19/2024

---
+++ src/Q2/Lp3D14/LpCll.java
@@ -0,0 +1,12 @@
+package Q2.Lp3D14;
+
+public class LpCll {
+    private int An;
+    private int Aj;
+    private int AC;
+    private int Mn;
+    private int Mj;
+    private int Mc;
+
+    public void LpCLl(int nw, int nj,int c)
+}
