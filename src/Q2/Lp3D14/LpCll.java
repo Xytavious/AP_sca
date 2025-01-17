e1a73f3edca3479de641daf00c3258ead5eb306b
williams.x <williams.x@stu.janesville.k12.wi.us>
2025-01-17 12:00:42 -0600

    1/17/2025

--- src/Q2/Lp3D14/LpCll.java
+++ src/Q2/Lp3D14/LpCll.java
@@ -7,6 +7,58 @@ public class LpCll {
     private int Mn;
     private int Mj;
     private int Mc;
+    private double av;
+    private double mv;
+    private int tot;
 
-   // public void LpCLl(int nw, int nj,int c)
+    public void LpCLl(int an, int aj, int ac, int mn, int mj, int mc){
+        An = an;
+        Aj = aj;
+        AC = ac;
+        Mn = mn;
+        Mj = mj;
+        Mc = mc;
+
+        av = ((double)An + Aj + AC) /3;
+        mv = ((double) Mn + Mj + Mc)/3;
+        tot = An + Aj + AC + Mn + Mj + Mc;
+
+    }
+
+    public int getAn() {
+        return An;
+    }
+
+    public int getAj() {
+        return Aj;
+    }
+
+    public int getAC() {
+        return AC;
+    }
+
+    public int getMn() {
+        return Mn;
+    }
+
+    public int getMj() {
+        return Mj;
+    }
+
+    public int getMc() {
+        return Mc;
+    }
+
+    public double getAv() {
+        return av;
+    }
+
+    public double getMv() {
+        return mv;
+    }
+
+    public int getTot() {
+        return tot;
+    }
+
 }
--- src/Q2/Lp3D14/lp14.java
+++ src/Q2/Lp3D14/lp14.java
@@ -1,8 +1,31 @@
 package Q2.Lp3D14;
+import java .util.Scanner;
+
 
 public class lp14 {
     public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
         System.out.println("Awbery \t Martinez");
-        System.out.println("New York");
+        int an = wow.nextInt();
+        System.out.println("");
+        int aj = wow.nextInt();
+        int ac = wow.nextInt();
+        int mn = wow.nextInt();
+        int mj = wow.nextInt();
+        int mc = wow.nextInt();
+
+
+        int some;
+        LpCll pop = new LpCll();
+
+         .out.println("Awbery \t Martinez");
+        System.out.println("New York \t ");
+        System.out.println("New Jersey \t 89008 \t 121032");
+        System.out.println("Connecticut \t 213451 \t 231034");
+
+        System.out.println("Election Results for New York:");
+        System.out.println("Awbrey: \t 314159");
+        System.out.println("Martinez: \t ");
+
     }
 }
