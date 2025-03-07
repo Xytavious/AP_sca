ca53ffacfb39dd6a11fed05d9ec5285c80dac475
williams.x <williams.x@stu.janesville.k12.wi.us>
2025-03-07 13:44:51 -0600

    2/10/2025

---
+++ src/Q3/Cl470a/CL470a.java
@@ -0,0 +1,5 @@
+package Q3.Cl470a;
+
+public class CL470a {
+    private int
+}
---
+++ src/Q3/Cl470a/prog470a.java
@@ -0,0 +1,21 @@
+package Q3.Cl470a;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class prog470a {
+    public static void main(String[] args) {
+        try {
+            Scanner file = new Scanner(new File("Langdat/FILENAME"));
+
+
+            while (file.hasNext()){
+
+            }
+        }catch (IOException e){
+            System.out.println("Error: " + e.getStackTrace());
+        }
+    }
+
+}
