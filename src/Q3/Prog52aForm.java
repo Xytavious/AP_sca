2fdcb28effed06f048453de73ee4d7b1089a719e
williams.x <williams.x@stu.janesville.k12.wi.us>
2025-03-13 13:47:58 -0500

    2/10/2025

---
+++ src/Q3/Prog52aForm.java
@@ -0,0 +1,16 @@
+package Q3;
+
+import javax.swing.*;
+
+public class Prog52aForm {
+    private JPanel MainPanel;
+    private JButton button1;
+    private JButton button2;
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Prog52a");
+        frame.setContentPane(new Prog52aForm().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.);
+
+    }
+}
