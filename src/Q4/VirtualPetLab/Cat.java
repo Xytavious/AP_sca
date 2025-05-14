6acfa7fb7752cccf4b51605c5eb01cd10fa797b3
williams.x <williams.x@stu.janesville.k12.wi.us>
2025-05-14 12:27:06 -0500

    5/9/2025

--- src/Q4/VirtualPetLab/Cat.java
+++ src/Q4/VirtualPetLab/Cat.java
@@ -9,12 +9,38 @@ public class Cat extends Pet {
     @Override
     public void feed() {
         // TODO: Implement feeding behavior for Cat
-        // maby setHunger(getHunger()+10);
-    }
+          setHunger(getHunger()-10);
+          if (getHunger() < 0) {
+            setHunger(0);
+          setHappiness(getHappiness()+10);
+          if (getHappiness() > 100) {
+                setHappiness(100);
+          setEnergy(getEnergy()+30);
+          if (getEnergy() > 100) {
+                setEnergy(100);
+          }
+                }
+
+
+            }
 
     @Override
     public void play() {
         // TODO: Implement playing behavior for Cat
+            setHappiness(getHappiness()+30);
+            if (getHappiness() > 100) {
+                setHappiness(100);
+            }
+            setHunger(getHunger()+5);
+            if (getHunger() > 100) {
+                setHunger(100);
+            }
+            setEnergy(getEnergy()-30);
+            if (getEnergy() < 0) {
+                setEnergy(0);
+            }
+        }
+
     }
 
     @Override
